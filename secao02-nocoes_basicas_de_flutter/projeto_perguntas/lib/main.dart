d7d94e6065a91bc1e03506534dc89780a9952954
Henrique Augusto Lobo <henriqueaugustolobo.dev@gmail.com>
2022-01-17 10:57:24 -0300

    Using Column and connecting anonymous functions to button event

--- secao02-nocoes_basicas_de_flutter/projeto_perguntas/lib/main.dart
+++ secao02-nocoes_basicas_de_flutter/projeto_perguntas/lib/main.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PerguntaApp());
+  runApp(const PerguntaApp());
 }
 
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({ Key? key }) : super(key: key);
 
+  void responder(int numero) {
+    print("Pergunta $numero respondida");
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual é a sua cor preferida?",
+      "Qual é o seu animal favorito?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Perguntas",
           ),
         ),
-        body: Text(
-          "Olá, mundo",
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: const Text("Resposta 1"),
+              onPressed: () {
+                responder(1);
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Resposta 2"),
+              onPressed: () {
+                responder(2);
+              }, 
+            ),
+            ElevatedButton(
+              child: const Text("Resposta 3"),
+              onPressed: () {
+                responder(3);
+              }, 
+            ),
+          ],
         ),
       ),
     );
