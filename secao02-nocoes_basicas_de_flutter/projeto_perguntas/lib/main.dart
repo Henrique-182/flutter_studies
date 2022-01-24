035cae6aeffa63fb96491db87510f1776672c260
Henrique Augusto Lobo <henriqueaugustolobo.dev@gmail.com>
2022-01-24 14:44:29 -0300

    Avoiding accessing invalid question

--- secao02-nocoes_basicas_de_flutter/projeto_perguntas/lib/main.dart
+++ secao02-nocoes_basicas_de_flutter/projeto_perguntas/lib/main.dart
@@ -15,31 +15,38 @@ class PerguntaApp extends StatefulWidget {
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      "texto": "Qual é a sua cor preferida?",
+      "respostas": ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      "texto": "Qual é o seu animal favorito?",
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      "texto": "Qual é o seu instrutor favorito?",
+      "respostas": ['Maria', 'João', 'Leo', 'Pedro']
+    }
+  ];
 
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        "texto": "Qual é a sua cor preferida?",
-        "respostas": ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        "texto": "Qual é o seu animal favorito?",
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        "texto": "Qual é o seu instrutor favorito?",
-        "respostas": ['Maria', 'João', 'Leo', 'Pedro']
-      }
-    ];
-
-    List<String> respostas = perguntas[_perguntaSelecionada].cast()["respostas"];
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()["respostas"]
+        : [];
     List<Widget> widgetsDeRespostas = respostas
         .map((texto) => Resposta(texto: texto, onPressed: _responder))
         .toList();
@@ -51,12 +58,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
             "Perguntas",
           ),
         ),
-        body: Column(
+        body: temPerguntaSelecionada ? Column(
           children: [
-            Questao(texto: perguntas[_perguntaSelecionada]["texto"].toString()),
+            Questao(
+                texto: _perguntas[_perguntaSelecionada]["texto"].toString()),
             ...widgetsDeRespostas, // pega todos os elementos de 'widgetsDeRespostas' e as substitui nessa linha
           ],
-        ),
+        ) : null,
       ),
     );
   }
